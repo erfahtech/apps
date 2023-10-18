a864d3acb5bba668d0038377c23225538dc3383b
erditona <erditonaushaadam@gmail.com>
2023-10-18 22:10:20 +0700

    yes

--- js/post.js
+++ js/post.js
@@ -2,29 +2,4 @@
 // import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
 import PostSignUp from "./postfunction.js";
 
-// Inisialisasi Spinner
-const spinner = new Spinner({
-  lines: 12, // Jumlah garis dalam animasi loading
-  length: 10, // Panjang garis
-  width: 5, // Lebar garis
-  radius: 20, // Radius animasi
-  color: "blue", // Warna animasi
-});
-
-// Fungsi untuk menampilkan animasi loading
-function showLoading() {
-  const target = document.getElementById("loading");
-  spinner.spin(target);
-  target.style.display = "block";
-}
-
-// Fungsi untuk menyembunyikan animasi loading
-function hideLoading() {
-  const target = document.getElementById("loading");
-  spinner.stop();
-  target.style.display = "none";
-}
-
-// Panggil fungsi PostSignUp dengan animasi loading
-showLoading();
 PostSignUp();
