7191f0406d7f422c993a6b097790e6595e4965e5
aulianafahrian <aulianafahrian@gmail.com>
2023-10-18 16:13:04 +0700

    up

--- js/postfunction.js
+++ js/postfunction.js
@@ -3,6 +3,7 @@ import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
 import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
 
 export default function PostSignUp() {
+    alert("post");
     let target_url = "https://asia-southeast2-urse-project.cloudfunctions.net/urse-post";
     let tokenkey = "token";
     let tokenvalue = "c49482e6de1fa07a349f354c2277e11bc7115297a40a1c09c52ef77b905d07c4";
