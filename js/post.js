40e96d63e00783fa36db66e5c7e4390ab016f106
erditona <erditonaushaadam@gmail.com>
2023-10-19 21:17:23 +0700

    Merapikan cuy

--- js/post.js
+++ js/post.js
@@ -1,5 +1,5 @@
 // import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 // import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
-import PostSignUp from "./postfunction-login.js";
+import Login from "./postfunction-login.js";
 
-window.PostSignUp = PostSignUp;
+window.Login = Login;
