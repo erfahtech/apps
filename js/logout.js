ffe6359dcba08f6acc03cbaecc21ffc370dc18fe
aulianafahrian <aulianafahrian@gmail.com>
2023-10-19 16:39:30 +0700

    pudh logout

--- js/logout.js
+++ js/logout.js
@@ -1,25 +1,23 @@
-import { deleteCookie } from "https://jscroot.github.io/cookie/croot.js";
-// function logout() {
-//     document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-//     window.location.href = "./index.html";
-// }
+
 
 function logout() {
     Swal.fire({
-      title: "Are you sure?",
-      text: "You session will disappear!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Logout",
+        title: "Are you sure?",
+        text: "You session will disappear!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Logout",
     }).then((result) => {
-      if (result.isConfirmed) {
-        // localStorage.removeItem("token");
+        if (result.isConfirmed) {
+            // localStorage.removeItem("token");
 
-        deleteCookie("token");
+            // Menghapus toke dari cookie
+            document.cookie =
+                "token= ; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 
-        window.location.href = "../../index.html";
-      }
+            window.location.href = "../../index.html";
+        }
     });
-  }
+}
