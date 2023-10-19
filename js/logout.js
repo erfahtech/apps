1d65023feae3cd6dc4dab84c0bd367b7011a0ea9
aulianafahrian <aulianafahrian@gmail.com>
2023-10-19 15:41:35 +0700

    up

--- js/logout.js
+++ js/logout.js
@@ -1,4 +1,21 @@
+// function logout() {
+//     document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+//     window.location.href = "./index.html";
+// }
+
 function logout() {
-    document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    window.location.href = "./index.html";
-}
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You session will disappear!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Logout",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.removeItem("token");
+        window.location.href = "../index.html";
+      }
+    });
+  }
