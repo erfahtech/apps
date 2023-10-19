c3417ddb39e9f0ac6bab7979714d5c0be04e266e
erditona <erditonaushaadam@gmail.com>
2023-10-19 22:08:47 +0700

    test

--- js/auth_halaman.js
+++ js/auth_halaman.js
@@ -4,9 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
   // Mendapatkan nilai cookie dengan nama "token" menggunakan fungsi getCookie
   const token = getCookie("token");
 
-  // Mendapatkan URL sebelumnya dari local storage (jika ada)
-  const previousPageURL = localStorage.getItem("previousPageURL");
-
   if (!token && previousPageURL) {
     Swal.fire({
       icon: "warning",
@@ -15,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
       confirmButtonText: "OK",
     }).then(() => {
       // Redirect ke halaman login
-      window.location.href = "https://github.com/erfahtech/apps/pages/auth/login.html";
+      window.location.href = erfahtech / apps / pages / auth / login.html;
     });
   }
 });
--- js/auth_login.js
+++ js/auth_login.js
@@ -1,48 +1,21 @@
-// import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   // Mendapatkan nilai cookie dengan nama "token" menggunakan fungsi getCookie
-//   const token = getCookie("token");
-
-//   // Mendapatkan URL sebelumnya dari local storage (jika ada)
-//   const previousPageURL = localStorage.getItem("previousPageURL");
-
-//   // Jika ada token, kembalikan ke halaman sebelumnya
-//   if (token && previousPageURL) {
-//     Swal.fire({
-//       icon: "info",
-//       title: "Informasi",
-//       text: "Anda sudah login. Kembali ke halaman sebelumnya.",
-//       confirmButtonText: "OK",
-//     }).then(() => {
-//       // Redirect ke halaman sebelumnya
-//       window.location.href = previousPageURL;
-//     });
-//   }
-// });
-
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
 document.addEventListener("DOMContentLoaded", function () {
+  // Mendapatkan nilai cookie dengan nama "token" menggunakan fungsi getCookie
   const token = getCookie("token");
+
+  // Mendapatkan URL sebelumnya dari local storage (jika ada)
   const previousPageURL = localStorage.getItem("previousPageURL");
 
-  if (!token && previousPageURL) {
-    Swal.fire({
-      icon: "warning",
-      title: "Peringatan",
-      text: "Anda belum login !",
-      confirmButtonText: "OK",
-    }).then(() => {
-      window.location.href = "https://erfahtech.github.io/apps/pages/auth/login.html";
-    });
-  } else if (token && previousPageURL) {
+  // Jika ada token, kembalikan ke halaman sebelumnya
+  if (token && previousPageURL) {
     Swal.fire({
       icon: "info",
       title: "Informasi",
       text: "Anda sudah login. Kembali ke halaman sebelumnya.",
       confirmButtonText: "OK",
     }).then(() => {
+      // Redirect ke halaman sebelumnya
       window.location.href = previousPageURL;
     });
   }
