12da5d91f4a60ffbbd97c8c420e165d5fdcb22c0
erditona <erditonaushaadam@gmail.com>
2023-10-22 15:51:47 +0700

    test

--- js/postfunction-signUp.js
+++ js/postfunction-signUp.js
@@ -9,6 +9,16 @@ export default function postSignUp() {
     password: getValue("passwordsignup"),
   };
 
+  // Validasi isian tidak boleh kosong
+  if (!email || !username || !password) {
+    Swal.fire({
+      icon: "error",
+      title: "Signup Failed",
+      text: "Please fill in all fields.",
+    });
+    return;
+  }
+
   fetch(target_url, {
     method: "POST",
     headers: {
