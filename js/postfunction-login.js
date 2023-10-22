ca4e8085db2cde381c54635b3da6b37f4bc271e5
erditona <erditonaushaadam@gmail.com>
2023-10-22 16:21:58 +0700

    testLagi

--- js/postfunction-login.js
+++ js/postfunction-login.js
@@ -20,33 +20,36 @@ export default function postLogin() {
   // Show the loading animation
   loadingElement.style.display = "block";
 
-  let target_url = "https://asia-southeast2-urse-project.cloudfunctions.net/urse-post";
-  let tokenkey = "token";
-  let tokenvalue = "c49482e6de1fa07a349f354c2277e11bc7115297a40a1c09c52ef77b905d07c4";
-  let datainjson = {
-    email: email,
-    password: password,
-  };
+  setTimeout(() => {
+    // Simulate a loading delay (you can adjust the time as needed)
+    let target_url = "https://asia-southeast2-urse-project.cloudfunctions.net/urse-post";
+    let tokenkey = "token";
+    let tokenvalue = "c49482e6de1fa07a349f354c2277e11bc7115297a40a1c09c52ef77b905d07c4";
+    let datainjson = {
+      email: email,
+      password: password,
+    };
 
-  fetch(target_url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(datainjson),
-  })
-    .then((response) => response.json())
-    .then((result) => {
-      responseData(result);
+    fetch(target_url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(datainjson),
     })
-    .catch((error) => {
-      // Handle errors (e.g., network issues)
-      console.error("Error:", error);
-    })
-    .finally(() => {
-      // Hide the loading animation when the request is done (whether successful or failed)
-      loadingElement.style.display = "none";
-    });
+      .then((response) => response.json())
+      .then((result) => {
+        responseData(result);
+      })
+      .catch((error) => {
+        // Handle errors (e.g., network issues)
+        console.error("Error:", error);
+      })
+      .finally(() => {
+        // Hide the loading animation when the request is done (whether successful or failed)
+        loadingElement.style.display = "none";
+      });
+  }, 2000); // Simulate a 2-second loading time (adjust as needed)
 }
 
 function responseData(result) {
