b1b4ff7a7c22cdefb5990a2eb4cd7c391f032336
erditona <erditonaushaadam@gmail.com>
2023-10-24 17:04:41 +0700

    fix-add-insertDevicefunction

--- js/postfunction-login.js
+++ js/postfunction-login.js
@@ -22,7 +22,7 @@ export default function postLogin() {
   // Simulate a loading delay (you can adjust the time as needed)
   let target_url = "https://asia-southeast2-urse-project.cloudfunctions.net/urse-post";
   let tokenkey = "token";
-  let tokenvalue = "c49482e6de1fa07a349f354c2adsad277e11bc7115297a40a1c09c52ef77b905d07c4";
+  let tokenvalue = "c49482e6de1fa07a349f354c2277e11bc7115297a40a1c09c52ef77b905d07c4";
   let datainjson = {
     email: email,
     password: password,
